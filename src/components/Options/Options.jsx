de93a5138ed289eafba67aef8aded75a4aa480fd
Panther1986 <polzovatel77@gmail.com>
2024-03-16 12:58:47 +0200

    update

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import css from "./Options.module.css";
+
+const Options = ({ onClickGood, onClickNeutral, onClickBad, onClickReset }) => {
+  return (
+    <div className={css.btnContainer}>
+      <button className={css.btnOption} type="button" onClick={onClickGood}>
+        Good
+      </button>
+      <button className={css.btnOption} type="button" onClick={onClickNeutral}>
+        Neutral
+      </button>
+      <button className={css.btnOption} type="button" onClick={onClickBad}>
+        Bad
+      </button>
+      <button className={css.btnOption} type="button" onClick={onClickReset}>
+        Reset
+      </button>
+    </div>
+  );
+};
+export default Options;
